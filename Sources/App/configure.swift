91e3ede47d9e963675bbc217deba8a8711ef610a
Kanstantsin Bucha <kanstantsin.bucha@icloud.com>
2022-08-13 19:02:31 +0200

    Add config for a local network debug

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,6 +8,8 @@ public func configure(_ app: Application) throws {
     try database(app)
 
     app.http.server.configuration.port = 4040
+    // Uncomment to serve requests from local network
+//    app.http.server.configuration.address = .hostname("192.168.0.193", port: 4040)
     app.routes.defaultMaxBodySize = "500kb"
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
