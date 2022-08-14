42d7d8fa0805bfda2e2ba6be90dd2eea73d7734f
Kanstantsin Bucha <kanstantsin.bucha@icloud.com>
2022-08-14 08:21:23 +0200

    Add limit to simultaneous firmware updates

--- Sources/App/Controllers/FirmwareUpdateController.swift
+++ Sources/App/Controllers/FirmwareUpdateController.swift
@@ -9,6 +9,8 @@ import Foundation
 import Vapor
 
 public class FirmwareUpdateController: Service {
+    private static let maxSimultaneousUpdatesCount = 20
+    private static var runningUpdatesCount = 0
     public static func getFirmwareUpdate(req: Request) throws -> Response {
         let query = try req.query.decode(UpdateFirmware.self)
         let directory = service(FileSystem.self).firmwareUpdatePath
@@ -22,13 +24,18 @@ public class FirmwareUpdateController: Service {
         guard deviceVersion < latestVersion else {
             throw Abort(.noContent)
         }
+        guard runningUpdatesCount < maxSimultaneousUpdatesCount else {
+            throw Abort(.tooManyRequests)
+        }
+        runningUpdatesCount += 1
         let id = UUID()
         log.event("""
-            Requested Update: \(id), deviceId: \(query.deviceId) \
+            Requested Update: \(id), simultaneous: \(runningUpdatesCount), deviceId: \(query.deviceId) \
             deviceVersion: \(deviceVersion), latestVersion: \(latestVersion) at file: \(path)
             """
         )
         let response = req.fileio.streamFile(at: path) { result in
+            runningUpdatesCount -= 1
             log.event("Finished Update \(id) with result: \(result)")
         }
         return response
@@ -49,7 +56,7 @@ public class FirmwareUpdateController: Service {
                 }
                 let fileName = (itemPath as NSString).deletingPathExtension
                 let parts = fileName.split(separator: "_")
-                log.info("itemPath splitted: \(parts)")
+                log.info("Firmware file name splitted: \(parts)")
                 guard parts.count == 2, let version = try? SemanticVersion(string: String(parts[1])) else {
                     log.failure("Found a firmware file with invalid format: \(itemPath)")
                     return nil
